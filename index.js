d2f61852a68b8b7dd79165374a91b3a003dc9f50
arshad <jahan665577@gmail.com>
2022-01-16 02:15:12 -0800

    nodejs

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+
+const data=require('./data.json')
+const app=express();
+
+const port=process.env.PORT || 3000
+
+app.get('/',(req,res)=>
+{
+    res.send('how are you')
+})
+
+app.get('/player',(req,res)=>{
+    res.send(data)
+})
+
+console.log(data)
+
+app.listen(port,()=>console.log("running on:"+port))
